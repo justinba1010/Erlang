12785aaa76d62ec2fce954cec2b66be313307fd6
Justin Baum <justinba1010@gmail.com>
2018-04-05 14:08:50 -0400

    Playing with math fumath funs.

--- mathdemo.erl
+++ mathdemo.erl
@@ -1,10 +1,5 @@
-%% @author Simon St.Laurent <simonstl@simonstl.com> [http://simonstl.com]
-%% @doc mathdemo
-%% @reference from <a href= "http://shop.oreilly.com/product/0636920025818.do" >
-%% Introducing Erlang</a>,
-%% O'Reilly Media, Inc., 2017.
-%% @copyright 2017 by Simon St.Laurent
-%% @version 0.1
+%% @author Justin Baum
+%% @doc I moved from falling velocities and have been playing with math identities, so no need for the copyrights anymore
 
 
 -module(mathdemo).
