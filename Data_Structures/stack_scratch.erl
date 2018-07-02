522962b8a4488b0a05f4adf4f8b5dbff3c7b862f
Justin Baum <justinba1010@gmail.com>
2018-07-02 16:03:40 -0400

    Stack to List

--- Data_Structures/stack_scratch.erl
+++ Data_Structures/stack_scratch.erl
@@ -1,5 +1,5 @@
 -module(stack_scratch).
--export([new/0,push/2,pop/1]).
+-export([new/0,push/2,pop/1,stackToList/1]).
 
 %I think I figured out how to make a list/stack from scratch
 %I was thinking of it mathematically, and thought of it as nested tuples over and over, the one painful thing would be traversing, and reversing where you would need to create a new one every time.
@@ -14,3 +14,8 @@ pop({}) -> empty;
 pop(List) -> %Use pattern matching to get head, and rest of stack.
   List.
 %%%   {Head, NewStack} = pop(OldStack). %%%
+stackToList(Stack) -> stackToList(Stack,[]).
+
+stackToList({},List) -> List;
+stackToList({Data,Stack},List) ->
+  stackToList(Stack, [Data]++List).
