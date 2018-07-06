7bb6785394528259b26ad4619cad3a3cc54aed1c
Justin Baum <justinba1010@gmail.com>
2018-07-06 13:39:33 -0400

    Binary Search Tree finished.

--- Data_Structures/bst_scratch.erl
+++ Data_Structures/bst_scratch.erl
@@ -1,9 +1,11 @@
 -module(bst_scratch).
--export([new/0,push/2]).
+-export([new/0,push/2,inTree/2, remove/2]).
 
 new() -> {null,{},{}}.
 
-push({null,_,_}, Data) -> {Data,{},{}};\
+push({null,_,_}, Data) -> {Data,{},{}};
+push({},Data) ->
+  {Data,{},{}};
 push({Data,Left,Right}, Value) when Value < Data ->
   {Data,push(Left,Value),Right};
 push({Data,Left,Right}, Value) ->
@@ -14,20 +16,31 @@ inTree({Data,_,_}, Data) ->
   true;
 inTree({Data,Left,_}, Value) when Value < Data ->
   inTree(Left,Value);
-inTree({Data,_,Right}, Value) ->
+inTree({_,_,Right}, Value) ->
   inTree(Right,Value).
 
 
+minRight({Data,{},{}}) ->
+  Data;
+minRight({_,Left,_}) ->
+  minRight(Left).
 
-remove({},Value) -> {};
-remove({Data,_,_}, Data) ->
-  %Need algorithm to take bottom of one tree, bring up, etc
-  %For now chop tree from there
-  %push rest into new tree % function remove readd
-  {}
+%End of Tree
+remove({},_) -> {};
+%Remove Leaf
+remove({Data,{},{}}, Data) ->
+  {};
+%One child Right
+remove({Data,Right,{}}, Data) ->
+  Right;
+%One child Left
+remove({Data,Left,{}}, Data) ->
+  Left;
+%Two children
+remove({Data,Left,Right}, Data) ->
+  Min = minRight(Left),
+  {Min, remove(Left,Min), Right};
 remove({Data,Left,Right}, Value) when Value < Data ->
   {Data,remove(Left, Value),Right};
 remove({Data,Left,Right}, Value) ->
   {Data,Left,remove(Right, Value)}.
-
-remove_readd() -> {}.
