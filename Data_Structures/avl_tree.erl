238d158c0b119f9d60537ee9b709e06baec5999d
Justin Baum <justinba1010@gmail.com>
2018-07-11 13:01:33 -0400

    Added Search

--- Data_Structures/avl_tree.erl
+++ Data_Structures/avl_tree.erl
@@ -1,4 +1,4 @@
-  %% @author Justin Baum <jabaum@cec.sc.edu> [http://github.com/justinba1010/]
+%% @author Justin Baum <jabaum@cec.sc.edu> [http://github.com/justinba1010/]
 %% @doc AVL Tree Implementation
 %% @copyright 2018 by Justin Baum
 %% @version 0.1
@@ -7,7 +7,7 @@
 
 
 -module(avl_tree).
--export([new/0,push/3,inTree/2, remove/2, rotate/1, depth/1]).
+-export([new/0,push/3,inTree/2, remove/2, rotate/1, depth/1, balance_tree/1, search/2]).
 %After reading LYSEFGG
 %Decided needs to be cleaner
 % @doc new() -> new Tree
@@ -18,76 +18,90 @@ new() -> {null,null,{},{},0}.
 % O(n log n)
 push({null,null,{},{},0}, Key, Value) -> {Key, Value,{},{},0};
 push({},Key, Value) ->
-  {Key, Value, {},{},0};
+{Key, Value, {},{},0};
 push({Key, Value ,Left,Right,_}, NewKey, NewValue) when NewKey < Key ->
-  NewLeft = push(Left,NewKey, NewValue),
-  rotate({Key,Value, NewLeft ,Right,depth(Right)-depth(NewLeft)});
+NewLeft = push(Left,NewKey, NewValue),
+rotate({Key,Value, NewLeft ,Right,depth(Right)-depth(NewLeft)});
 push({Key, Value ,Left,Right,_}, NewKey, NewValue) ->
-  NewRight = push(Right,NewKey, NewValue),
-  rotate({Key,Value, Left, NewRight, depth(NewRight)-depth(Left)}).
+NewRight = push(Right,NewKey, NewValue),
+rotate({Key,Value, Left, NewRight, depth(NewRight)-depth(Left)}).
 
 % @doc inTree(Tree, Key) -> true or false depending on if in tree
 % O(log n) time:
 inTree({},_) -> false;
 inTree({Key,_,_,_,_}, Key) ->
-  true;
+true;
 inTree({Data,_,Left,_,_}, Value) when Value < Data ->
-  inTree(Left,Value);
+inTree(Left,Value);
 inTree({_,_,_,Right,_}, Value) ->
-  inTree(Right,Value).
+inTree(Right,Value).
 
 % @doc minRight(Tree) -> returns min value in tree as {Key, Value}
 % O(log n) time
-minRight({Key,Value,{},{},_}) ->
-  {Key, Value};
+minRight({Key,Value,{},_,_}) ->
+{Key, Value};
 minRight({_,_,Left,_,_}) ->
-  minRight(Left).
+minRight(Left).
 
+% @doc search(Tree, Value) -> {Key, Value}
+search({}, _) -> {error,not_found};
+search({Key, Value, _, _,_}, Key) -> {Key, Value};
+search({Key, _, Left, _, _}, AKey) when AKey < Key -> search(Left, AKey);
+search({_, _, _, Right, _}, AKey) -> search(Right, AKey).
 
 % @doc remove(Tree, Key) -> returns tree without value
-% O(n log n) time
+% A Fuck ton of time
 %End of Tree
 remove({},_) -> {};
 %Remove Leaf
 remove({_, _,{},{}}, _) ->
-  {};
+{};
 %One child Right
 remove({Key, _,{},Right,_}, Key) ->
-  Right;
+rotate(Right);
 %One child Left
 remove({Key,_,Left,{},_}, Key) ->
-  Left;
+rotate(Left);
 %Two children
 remove({Key,_,Left,Right, _}, Key) ->
-  {NewKey, NewValue} = minRight(Right),
-  NewRight = remove(Right,NewKey),
-  rotate({NewKey, NewValue, Left, NewRight, depth(NewRight)-depth(Left)});
+{NewKey, NewValue} = minRight(Right),
+NewRight = remove(Right,NewKey),
+rotate({NewKey, NewValue, Left, NewRight, 0});
 remove({Key,Value,Left,Right,_}, RKey) when RKey < Key ->
-  rotate({Key,Value,remove(Left, RKey),Right, depth(Right)-depth(remove(Left, RKey))});
+rotate({Key,Value,remove(Left, RKey),Right, 0});
 remove({Key, Value,Left,Right, _}, RKey) ->
-  rotate({Key,Value,Left,remove(Right, RKey), depth(remove(Right, RKey))-depth(Left)}).
+rotate({Key,Value,Left,remove(Right, RKey), 0}).
 
 % @doc pattern matching rotations of tree
 % O(1) time
 %Left Rotation
 
 rotate({AKey, AValue, R1, {BKey, BValue, R2, {CKey, CValue, R3, R4,_}, 1}, 2}) ->
-  {BKey, BValue, {AKey,AValue,R1,R2,0},{CKey, CValue,R3,R4,0},0};
+{BKey, BValue, {AKey,AValue,R1,R2,0},{CKey, CValue,R3,R4,0},0};
 %Right Rotation
 rotate({AKey, AValue, {BKey, BValue, {CKey, CValue, R4, R3,0}, R2, -1}, R1, -2}) ->
-  {BKey, BValue, {CKey,CValue,R4,R3,0},{AKey, AValue,R2,R1,0},0};
+{BKey, BValue, {CKey,CValue,R4,R3,0},{AKey, AValue,R2,R1,0},0};
 %Left-Right Rotation
 rotate({CKey, CValue,{AKey,AValue,R2,{BKey, BValue,R3,R4,0},1},R1,-2}) ->
-  {BKey, BValue, {AKey,AValue,R2,R3,0},{CKey, CValue,R4,R1,0},0};
+{BKey, BValue, {AKey,AValue,R2,R3,0},{CKey, CValue,R4,R1,0},0};
 %Right-Left Rotation
 rotate({AKey, AValue, R1, {CKey, CValue, {BKey, BValue, R2, R3,_},R4, -1}, 2}) ->
-  {BKey, BValue, {AKey,AValue,R1,R2,0},{CKey, CValue,R3,R4,0},0};
+{BKey, BValue, {AKey,AValue,R1,R2,0},{CKey, CValue,R3,R4,0},0};
 rotate(Tree) ->
-  Tree.
+Tree.
+
+
+% @doc Balance Tree
+% O(n) time I believe
+balance_tree({}) -> {};
+balance_tree({Key, Value, Left, Right, _}) ->
+NewLeft = balance_tree(Left),
+NewRight = balance_tree(Right),
+rotate({Key, Value, NewLeft, NewRight, depth(NewRight) - depth(NewLeft)}).
 
 % @doc Depth function, tail recursive
 % O(n) time
 depth(Tree) -> depth(Tree,0).
 depth({},Acc) -> Acc;
 depth({_,_,Left,Right,_},Acc) ->
-  max(depth(Left,Acc+1), depth(Right, Acc+1)).
+max(depth(Left,Acc+1), depth(Right, Acc+1)).
