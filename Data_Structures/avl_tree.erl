33d4fc118adc89c80f238d74697f95ee5caf14af
Justin Baum <justinba1010@gmail.com>
2018-07-11 13:03:11 -0400

    Added indents...

--- Data_Structures/avl_tree.erl
+++ Data_Structures/avl_tree.erl
@@ -18,30 +18,30 @@ new() -> {null,null,{},{},0}.
 % O(n log n)
 push({null,null,{},{},0}, Key, Value) -> {Key, Value,{},{},0};
 push({},Key, Value) ->
-{Key, Value, {},{},0};
+  {Key, Value, {},{},0};
 push({Key, Value ,Left,Right,_}, NewKey, NewValue) when NewKey < Key ->
-NewLeft = push(Left,NewKey, NewValue),
-rotate({Key,Value, NewLeft ,Right,depth(Right)-depth(NewLeft)});
+  NewLeft = push(Left,NewKey, NewValue),
+  rotate({Key,Value, NewLeft ,Right,depth(Right)-depth(NewLeft)});
 push({Key, Value ,Left,Right,_}, NewKey, NewValue) ->
-NewRight = push(Right,NewKey, NewValue),
-rotate({Key,Value, Left, NewRight, depth(NewRight)-depth(Left)}).
+  NewRight = push(Right,NewKey, NewValue),
+  rotate({Key,Value, Left, NewRight, depth(NewRight)-depth(Left)}).
 
 % @doc inTree(Tree, Key) -> true or false depending on if in tree
 % O(log n) time:
 inTree({},_) -> false;
 inTree({Key,_,_,_,_}, Key) ->
-true;
+  true;
 inTree({Data,_,Left,_,_}, Value) when Value < Data ->
-inTree(Left,Value);
+  inTree(Left,Value);
 inTree({_,_,_,Right,_}, Value) ->
-inTree(Right,Value).
+  inTree(Right,Value).
 
 % @doc minRight(Tree) -> returns min value in tree as {Key, Value}
 % O(log n) time
 minRight({Key,Value,{},_,_}) ->
-{Key, Value};
+  {Key, Value};
 minRight({_,_,Left,_,_}) ->
-minRight(Left).
+  minRight(Left).
 
 % @doc search(Tree, Value) -> {Key, Value}
 search({}, _) -> {error,not_found};
@@ -55,53 +55,53 @@ search({_, _, _, Right, _}, AKey) -> search(Right, AKey).
 remove({},_) -> {};
 %Remove Leaf
 remove({_, _,{},{}}, _) ->
-{};
+  {};
 %One child Right
 remove({Key, _,{},Right,_}, Key) ->
-rotate(Right);
+  rotate(Right);
 %One child Left
 remove({Key,_,Left,{},_}, Key) ->
-rotate(Left);
+  rotate(Left);
 %Two children
 remove({Key,_,Left,Right, _}, Key) ->
-{NewKey, NewValue} = minRight(Right),
-NewRight = remove(Right,NewKey),
-rotate({NewKey, NewValue, Left, NewRight, 0});
+  {NewKey, NewValue} = minRight(Right),
+  NewRight = remove(Right,NewKey),
+  rotate({NewKey, NewValue, Left, NewRight, 0});
 remove({Key,Value,Left,Right,_}, RKey) when RKey < Key ->
-rotate({Key,Value,remove(Left, RKey),Right, 0});
+  rotate({Key,Value,remove(Left, RKey),Right, 0});
 remove({Key, Value,Left,Right, _}, RKey) ->
-rotate({Key,Value,Left,remove(Right, RKey), 0}).
+  rotate({Key,Value,Left,remove(Right, RKey), 0}).
 
 % @doc pattern matching rotations of tree
 % O(1) time
 %Left Rotation
 
 rotate({AKey, AValue, R1, {BKey, BValue, R2, {CKey, CValue, R3, R4,_}, 1}, 2}) ->
-{BKey, BValue, {AKey,AValue,R1,R2,0},{CKey, CValue,R3,R4,0},0};
+  {BKey, BValue, {AKey,AValue,R1,R2,0},{CKey, CValue,R3,R4,0},0};
 %Right Rotation
 rotate({AKey, AValue, {BKey, BValue, {CKey, CValue, R4, R3,0}, R2, -1}, R1, -2}) ->
-{BKey, BValue, {CKey,CValue,R4,R3,0},{AKey, AValue,R2,R1,0},0};
+  {BKey, BValue, {CKey,CValue,R4,R3,0},{AKey, AValue,R2,R1,0},0};
 %Left-Right Rotation
 rotate({CKey, CValue,{AKey,AValue,R2,{BKey, BValue,R3,R4,0},1},R1,-2}) ->
-{BKey, BValue, {AKey,AValue,R2,R3,0},{CKey, CValue,R4,R1,0},0};
+  {BKey, BValue, {AKey,AValue,R2,R3,0},{CKey, CValue,R4,R1,0},0};
 %Right-Left Rotation
 rotate({AKey, AValue, R1, {CKey, CValue, {BKey, BValue, R2, R3,_},R4, -1}, 2}) ->
-{BKey, BValue, {AKey,AValue,R1,R2,0},{CKey, CValue,R3,R4,0},0};
+  {BKey, BValue, {AKey,AValue,R1,R2,0},{CKey, CValue,R3,R4,0},0};
 rotate(Tree) ->
-Tree.
+  Tree.
 
 
 % @doc Balance Tree
 % O(n) time I believe
 balance_tree({}) -> {};
 balance_tree({Key, Value, Left, Right, _}) ->
-NewLeft = balance_tree(Left),
-NewRight = balance_tree(Right),
-rotate({Key, Value, NewLeft, NewRight, depth(NewRight) - depth(NewLeft)}).
+  NewLeft = balance_tree(Left),
+  NewRight = balance_tree(Right),
+  rotate({Key, Value, NewLeft, NewRight, depth(NewRight) - depth(NewLeft)}).
 
 % @doc Depth function, tail recursive
 % O(n) time
 depth(Tree) -> depth(Tree,0).
 depth({},Acc) -> Acc;
 depth({_,_,Left,Right,_},Acc) ->
-max(depth(Left,Acc+1), depth(Right, Acc+1)).
+  max(depth(Left,Acc+1), depth(Right, Acc+1)).
